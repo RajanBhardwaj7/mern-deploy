8d0e6a575813799d66a6689383646ada9ba9f762
RajanBhardwaj7 <rb28012003@gmail.com>
2025-01-16 01:52:26 +0530

    added backend api for frontend.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState(''); // Initialize the state with an empty string
 
   useEffect(() => {
-    fetch("http://localhost:4000")
+    fetch("https://mern-deploy-backend-7noa.onrender.com")
       .then(res => res.json())
       .then(data => setMessage(data.message)); // Update state with the message from the backend
   }, []); // Add empty dependency array to run only once on mount
